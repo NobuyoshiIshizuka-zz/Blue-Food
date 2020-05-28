d6b0ad1c308ab36be7110f1bfbf4ada0a539835e
Nobuyoshi Ishizuka <nobuyoshi_japa@hotmail.com>
2020-05-28 13:59:19 -0300

    Configurando o Spring-Boot

---
+++ deliciusfood/src/main/java/com/nobuwebdeveloper/deliciusfood/DeliciusfoodApplication.java
@@ -0,0 +1,13 @@
+package com.nobuwebdeveloper.deliciusfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliciusfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliciusfoodApplication.class, args);
+	}
+
+}
---
+++ deliciusfood/src/test/java/com/nobuwebdeveloper/deliciusfood/DeliciusfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nobuwebdeveloper.deliciusfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliciusfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
