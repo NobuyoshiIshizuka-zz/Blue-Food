c1fb01c266125225be1197850202b5ea97481fc7
Nobuyoshi Ishizuka <nobuyoshi_japa@hotmail.com>
2020-06-02 10:40:56 -0300

    add Class loginController

--- deliciusfood/src/main/java/com/nobuwebdeveloper/deliciusfood/DeliciusfoodApplication.java
+++ deliciusfood/src/main/java/com/nobuwebdeveloper/deliciusfood/DeliciusfoodApplication.java
@@ -1,4 +1,4 @@
-package com.nobuwebdeveloper.deliciusfood.deliciusfood;
+package com.nobuwebdeveloper.deliciusfood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
